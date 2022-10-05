ee4fbb69fb9d83f8b98a0c11a7445b50de008fbd
Irsyad Ibrahim <muhammadirsyadibrahim21@gmail.com>
2022-10-05 14:08:17 +0700

    Update Controller 0.1

--- Controller.java
+++ Controller.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import Model.User;
 import java.util.Calendar;
 
@@ -27,16 +26,27 @@ public class Controller {
                                 @PathVariable String location){
         return "My name is " + name + ". I am " + age + " years old. I am living in " + location;
     }
+    // Post Mapping, Request Body, Input JSON Output JSON
 
     @PostMapping("/users")
     public ResponseEntity printUser(@RequestBody User user){
         //System.out.println("Printing Object User Name : " + user.getName() + ", location: " +user.getLocation())+ ;
         //return "Berhasil! Data user "+ user.getName() + ", location: " +user.getLocation())+ ;
-        Calendar kalender = Calendar.getInstance();
+       Calendar kalender = Calendar.getInstance();
         int tahun_sekarang = kalender.get(Calendar.YEAR);
         user.setName(user.getName()+" tambahan");
         user.setTahun_lahir(tahun_sekarang - user.getAge());
         return new ResponseEntity(user, HttpStatus.OK);
+    }
+
+    @PostMapping("/users2")
+    public ResponseEntity infoUser(@RequestBody User user){
+
+        Calendar kalender = Calendar.getInstance();
+        int tahun_sekarang = kalender.get(Calendar.YEAR);
+        user.setAge(tahun_sekarang - user.getTahun_lahir());
+        return new ResponseEntity(user, HttpStatus.OK);
+
     }
 
 }
